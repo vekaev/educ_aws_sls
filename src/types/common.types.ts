60c68f232e4787d55fc665cf6d438c63398d132a
ypetrenko <vekaev4@gmail.com>
2023-02-12 17:41:02 +0100

    little refactoring and add serverless-domain-manager

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -4,14 +4,12 @@ import {
     APIGatewayProxyResult,
 } from 'aws-lambda';
 import middy from '@middy/core';
-import validator from '@middy/validator';
 
 export enum PolicyEffect {
     ALLOW = 'Allow',
     DENY = 'Deny',
 }
 
-export type ValidatorOptions = Parameters<typeof validator>[0];
 export type MiddlewareObj = middy.MiddlewareObj<
     APIGatewayProxyEvent,
     APIGatewayProxyResult
